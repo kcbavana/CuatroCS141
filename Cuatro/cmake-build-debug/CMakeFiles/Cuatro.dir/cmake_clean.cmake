ab6ad2b9b748ecc8c0febb11feb9ba95e7008f11
kcbavana <53190642+kcbavana@users.noreply.github.com>
2020-02-08 12:02:22 -0600

    Final Version of Cuatro
    Program Designed for CS 141, submitted on 2/7/2020

---
+++ Cuatro/cmake-build-debug/CMakeFiles/Cuatro.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cuatro.dir/main.cpp.obj"
+  "Cuatro.exe"
+  "Cuatro.exe.manifest"
+  "Cuatro.pdb"
+  "libCuatro.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cuatro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
